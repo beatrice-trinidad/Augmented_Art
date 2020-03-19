eff14dd975c5d5ff09ecff2c26f0023f8da4366f
VessoVit <vesselin@vv-studio.com>
2020-03-19 18:05:05 +0100

    Room final design, effects (KinoGlitch, analog glitch, lattice)

---
+++ Assets/Scripts/moveObj.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveObj : MonoBehaviour
+{
+    public Vector3 point;
+    //public Vector3 countVar;
+    public GameObject intrObjts;
+    public GameObject intrObjtsAnalog;
+    public GameObject intrWalls;
+    public GameObject intrWalls2;
+    public GameObject intrWalls3;
+
+    private Kino.AnalogGlitch analogGlitch;
+    private Kvant.Lattice lattice;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        analogGlitch = GetComponent<Kino.AnalogGlitch>();
+        lattice = GetComponent<Kvant.Lattice>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * 20 * Time.deltaTime, Space.Self);
+            //countVar = Vector3.up * 20 * Time.deltaTime;
+            
+        }
+        else if (Input.GetKey(KeyCode.D))
+
+        {
+            transform.Rotate(-Vector3.up * 20 * Time.deltaTime, Space.Self);
+
+        }
+        intrObjts.GetComponent<Kino.AnalogGlitch>().colorDrift = transform.rotation.y/5;
+        intrObjtsAnalog.GetComponent<Kino.DigitalGlitch>().intensity = transform.rotation.y/3;
+        //lattice.noiseElevation = transform.rotation.y;
+        intrWalls.GetComponent<Kvant.Lattice>().noiseElevation = transform.rotation.y*30;
+        intrWalls2.GetComponent<Kvant.Lattice>().noiseElevation = transform.rotation.y * 30;
+        intrWalls3.GetComponent<Kvant.Lattice>().noiseElevation = transform.rotation.y * 30;
+
+    }
+}
